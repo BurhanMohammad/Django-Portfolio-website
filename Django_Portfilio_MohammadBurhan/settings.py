6ae967bb3a0f8fdd752b431ee76a235f2d137719
Mohammad Burhan <104616403+BurhanMohammad@users.noreply.github.com>
2023-01-15 07:10:51 +0000

    commit

--- Django_Portfilio_MohammadBurhan/settings.py
+++ Django_Portfilio_MohammadBurhan/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
